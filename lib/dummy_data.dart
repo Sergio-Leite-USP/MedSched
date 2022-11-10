06af2eb3af68b92cb8be051755f798f67acd156e
Sergio Leite <sergioleitedev@gmail.com>
2022-11-10 19:16:04 -0300

    Erro ortografico em dummy_data

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -4,7 +4,7 @@ const DUMMY_MEDICOS = [
   Medico(id: 1, nome: "Vitor", especialidade: "Ginecologista"),
   Medico(id: 2, nome: "Marcelo", especialidade: "Otorrinolaringologista"),
   Medico(id: 3, nome: "Jesus", especialidade: "Médico de Almas"),
-  Medico(id: 4, nome: "Igor", especialidade: "gastroenterologista"),
+  Medico(id: 4, nome: "Igor", especialidade: "Gastroenterologista"),
   Medico(id: 5, nome: "Pedro", especialidade: "Oftalmologista"),
   Medico(id: 6, nome: "Renata", especialidade: "Oftalmologista"),
   Medico(id: 7, nome: "Gabriela", especialidade: "Obstetra"),
